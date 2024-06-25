f01c9555f7f7a4fa85d9fc9480333c45893e749e
ElkiIgolki <dianaderbina2@gmail.com>
2024-06-25 23:56:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = (19, 'Низя-низя', 3.5)
+print(immutable_var)
+# immutable_var[0] = 11
+# Кортеж относится к неизменяемому типу данных
+mutable_list = [34, 'February', 28, 0.4]
+mutable_list[0] = 64
+mutable_list[1] = 'Февраль'
+print(mutable_list)
